e98db8ab0d258cf1b08f832c3d0b21ed5229fff4
Craig Behnke <cbehnke@commander.finance>
2024-06-12 03:04:35 -0500

    feat: use OCI method to install the latest fleeting plugin
    https://docs.gitlab.com/runner/fleet_scaling/fleeting.html?tab=Google+Cloud#install-with-the-oci-registry-distribution

--- submodules/manager/manager-start-command.sh
+++ submodules/manager/manager-start-command.sh
@@ -1,3 +1,18 @@
+sudo docker run \
+    -v /runner/etc:/etc/gitlab-runner \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /runner/bin:/usr/local/bin \
+    -v /root/.ssh:/root/.ssh \
+    -v /etc/ssh:/etc/ssh \
+    -e TZ="${TIMEZONE}" \
+    -e DOCKER_HOST="unix:///run/docker.sock" \
+    -e FLEETING_PLUGIN_PATH="/usr/local/bin" \
+    --privileged \
+    --network host \
+    gitlab/gitlab-runner:latest \
+    fleeting install
+    # We want to install the fleeting plugin for the system before we run the system itself
+
 sudo docker run -d --name gitlab-runner --restart always \
     -v /runner/etc:/etc/gitlab-runner \
     -v /var/run/docker.sock:/var/run/docker.sock \
